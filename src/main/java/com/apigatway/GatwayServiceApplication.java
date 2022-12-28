79c20282f5ca1cc6c04bcdb8893c321036c8c24c
Sai Ram <g.sairam67@gmail.com>
2022-12-28 14:34:03 +0530

    Initial commit

---
+++ src/main/java/com/apigatway/GatwayServiceApplication.java
@@ -0,0 +1,15 @@
+package com.apigatway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class GatwayServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatwayServiceApplication.class, args);
+	}
+
+}
