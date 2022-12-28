94f56854b68b5cb33cce98463fe357d6ab283c09
Sai Ram <g.sairam67@gmail.com>
2022-12-28 23:49:02 +0530

    t

--- src/main/java/com/apigatway/GatwayServiceApplication.java
+++ src/main/java/com/apigatway/GatwayServiceApplication.java
@@ -3,6 +3,13 @@ package com.apigatway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import io.netty.resolver.DefaultAddressResolverGroup;
+import reactor.netty.http.client.HttpClient;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -11,5 +18,16 @@ public class GatwayServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GatwayServiceApplication.class, args);
 	}
+	
+	
+    @Bean
+    @Primary
+    public WebClient webClient() {
+        HttpClient httpClient = HttpClient.create().resolver(DefaultAddressResolverGroup.INSTANCE);
+        return WebClient.builder()
+                .clientConnector(new ReactorClientHttpConnector(httpClient))
+                .build();
+    }
+
 
 }
